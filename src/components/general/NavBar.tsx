32e6d93c90df2762746d431e59258225f5afb83f
Emile Christie <emile.christie.pereira@ccc.ufcg.edu.br>
2025-03-28 21:29:49 -0300

    deixando a navbar fixa no topo

--- src/components/general/NavBar.tsx
+++ src/components/general/NavBar.tsx
@@ -5,7 +5,7 @@ import { Link, Text } from "@chakra-ui/react"
 
 export const NavBar = () => {
     return (
-      <Box position={"sticky"} zIndex={"999"} px={8} height={"12vh"} w={"full"} bgColor={"blue.800"} alignContent={"center"} color={"gray.200"}>
+      <Box position={"fixed"} zIndex={"999"} px={8} height={"12vh"} w={"full"} bgColor={"blue.800"} alignContent={"center"} color={"gray.200"}>
         <Flex w={"full"} gap={8} placeContent={"space-between"}>
             <Flex alignItems={"center"} gapX={2}>
                 <BookCopy size={"28"}/>
